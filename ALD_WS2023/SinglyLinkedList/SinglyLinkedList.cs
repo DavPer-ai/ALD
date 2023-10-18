56e574294811677727ee01311a055d3ab06389b2
DavPer-ai <115530959+DavPer-ai@users.noreply.github.com>
2023-10-18 10:04:37 +0200

    Add ALD Project

---
+++ ALD_WS2023/SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList
+{
+    internal class Node<T>
+    {
+        public Node<T> m_next { get; set; }
+
+        T m_data {  get; set; }
+    }
+    public class SinglyLinkedList<T>
+    {
+        Node<T> m_head { get; set; }
+        Node<T> m_last { get; set; }
+        int m_cnt { get; set; }
+
+        void Add(T item)
+        {
+            Node<T> tmp = new Node<T>();
+            if(m_head== null)
+            {
+                m_head = tmp;
+                m_last = tmp;
+                return;
+            }
+
+            m_last.m_next = tmp;
+            m_last = tmp;
+        }
+
+    }
+}
