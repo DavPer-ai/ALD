4e9f6abd250d0b2bd755c71a53be10ef53bf3a49
DavPer-ai <pernegger.david@gmail.com>
2023-10-24 13:21:49 +0200

    add tests

--- ALD_WS2023/SLL_TestApp/Program.cs
+++ ALD_WS2023/SLL_TestApp/Program.cs
@@ -12,7 +12,7 @@ namespace SLL_TestApp
     {
         static void Main(string[] args)
         {
-            //TestSinglyLinkedList();
+            TestSinglyLinkedList();
 
             TestSinglyLinkedListWithDictionary();
         }
@@ -30,19 +30,34 @@ namespace SLL_TestApp
 
             var a2 = test.IsObjectAtIndex("a", 1);
 
-            var b1 = test.FindByIndex(2);
+            try
+            {
+                var b1 = test.FindByIndex(1);
 
-            var b2 = test.FindByIndex(3);
+                var b2 = test.FindByIndex(2);
 
-            var b3 = test.FindByIndex(2);
+                var b3 = test.FindByIndex(3);
 
-            var count = test.Count();
+                var count = test.Count();
 
-            var c1 = test.FindByIndex(1);
+                var c1 = test.FindByIndex(1);
 
-            test.Remove("b");
+                test.Remove("b");
 
-            var c2 = test.FindByIndex(1);
+                var c2 = test.FindByIndex(1);
+
+                var c3 = test.Count();
+
+                test.Clear();
+
+                var d1 = test.Count();
+
+                var d2 = test.FindByIndex(0);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         private static void TestSinglyLinkedListWithDictionary()
